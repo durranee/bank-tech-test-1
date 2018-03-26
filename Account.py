21e6ea380afe18fd86abedbd251b3ba98b318827
Noel Vock <noel1uk@gmail.com>
2018-03-26 16:59:44 +0100

    refactor: removed save transaction logic from deposit and created its own method

--- Account.py
+++ Account.py
@@ -1,18 +1,23 @@
 import datetime
 
 class Account:
+
     def __init__(self):
         self.balance = 0
         self.history = []
 
+    def save_transaction(self, date, credit, debit, balance):
+        self.history.append([
+            date,
+            credit,
+            debit,
+            balance
+            ])
+
     def deposit(self, amount):
         self.balance += amount
-        self.history.append([
-            datetime.date.today().strftime("%d/%m/%Y"),
-            amount,
-            False,
-            self.balance
-            ])
+        date = datetime.date.today().strftime("%d/%m/%Y")
+        self.save_transaction(date, amount, False, self.balance)
 
     def withdraw(self, amount):
         self.balance -= amount
