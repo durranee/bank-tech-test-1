dc4c5de116b6c04b3a8b73e97c7f3e7f88a8313c
Noel Vock <noel1uk@gmail.com>
2018-03-26 16:35:32 +0100

    test: ensure account historyappends correct list on deposit

--- test_Account.py
+++ test_Account.py
@@ -1,4 +1,6 @@
 from Account import Account
+from freezegun import freeze_time
+import datetime
 
 def test_Account():
     account = Account()
@@ -9,6 +11,10 @@ def test_Account():
     account.withdraw(500)
     assert account.balance == 500
 
+@freeze_time("2018-01-18")
 def test_Account_history():
+    assert datetime.datetime.now() == datetime.datetime(2018, 1, 18)
     account = Account()
     assert account.history == []
+    account.deposit(1000)
+    assert account.history == [['18/01/2018', 1000.00, False, 1000.00]]
