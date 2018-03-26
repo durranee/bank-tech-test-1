4ccbeba4a9c4a1317ccc08596cfe9d044c2f9d8d
Noel Vock <noel1uk@gmail.com>
2018-03-26 15:06:19 +0100

    test: ensure account history is empty on instantiation

--- test_Account.py
+++ test_Account.py
@@ -1,4 +1,5 @@
 from Account import Account
+
 def test_Account():
     account = Account()
     assert isinstance(account, Account)
@@ -7,3 +8,7 @@ def test_Account():
     assert account.balance == 1000
     account.withdraw(500)
     assert account.balance == 500
+
+def test_Account_history():
+    account = Account()
+    assert account.history == []
