d311bb2e6abba4f58ac901e6225d142af00ae50e
Noel Vock <noel1uk@gmail.com>
2018-03-27 12:05:05 +0100

    refactor: use fixtures to keep tests DRY

--- test_Account.py
+++ test_Account.py
@@ -1,20 +1,26 @@
 from Account import Account
 from freezegun import freeze_time
 import datetime
+import pytest
 
-def test_Account():
+@pytest.fixture(scope="function")
+@freeze_time("2018-01-18")
+def account():
     account = Account()
-    assert isinstance(account, Account)
-    assert account.balance == 0
     account.deposit(1000)
-    assert account.balance == 1000
+    return account
+
+def test_Account(account):
+    assert isinstance(account, Account)
+
+def test_Account_deposit(account):
+    account.deposit(1000)
+    assert account.balance == 2000
+
+def test_Account_withdraw(account):
     account.withdraw(500)
     assert account.balance == 500
 
-@freeze_time("2018-01-18")
-def test_Account_history():
-    assert datetime.datetime.now() == datetime.datetime(2018, 1, 18)
-    account = Account()
-    assert account.history == []
-    account.deposit(1000)
+def test_Account_history(account):
+    account
     assert account.history == [['18/01/2018', 1000.00, False, 1000.00]]
