c7ea881f7126588ff418602aa7d396846bb9decf
Noel Vock <noel1uk@gmail.com>
2018-03-28 18:56:54 +0100

    test: ensure 'false' is changed to ''

--- test_Processor.py
+++ test_Processor.py
@@ -34,3 +34,13 @@ def test_Processor_to_string():
         ['13/01/2012', '2000', 'False', '3000'],
         ['14/01/2012', 'False', '500', '2500']
     ]
+
+def test_Processor_remove_false():
+    process = Processor(mock_account.history)
+    process.to_string()
+    process.remove_false()
+    assert process.partial_history == [
+        ['10/01/2012', '1000', '', '1000'],
+        ['13/01/2012', '2000', '', '3000'],
+        ['14/01/2012', '', '500', '2500']
+    ]
