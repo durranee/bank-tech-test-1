138d6504cdb4a7d7cda3053b49b0579db8229055
Noel Vock <noel1uk@gmail.com>
2018-03-30 12:59:31 +0100

    refactor: changed variable name to avoid python built in method

--- test_Formatter.py
+++ test_Formatter.py
@@ -9,8 +9,8 @@ mock_account.history = [
 
 
 def test_Formatter():
-    format = Formatter(mock_account.history)
-    assert format.formated_history == """
+    formatter = Formatter(mock_account.history)
+    assert formatter.formated_history == """
     date || credit || debit || balance
     14/01/2012 || || 500.00 || 2500.00
     13/01/2012 || 2000.00 || || 2500.00
@@ -18,9 +18,9 @@ def test_Formatter():
 
 
 def test_Formatter_reverse():
-    format = Formatter(mock_account.history)
-    format.reverse()
-    assert format.partial_history == [
+    formatter = Formatter(mock_account.history)
+    formatter.reverse()
+    assert formatter.partial_history == [
         ['14/01/2012', '', 500, 2500],
         ['13/01/2012', 2000, '', 3000],
         ['10/01/2012', 1000, '', 1000]
@@ -28,9 +28,9 @@ def test_Formatter_reverse():
 
 
 def test_Formatter_format_integer():
-    format = Formatter(mock_account.history)
-    format.format_integer()
-    assert format.partial_history == [
+    formatter = Formatter(mock_account.history)
+    formatter.format_integer()
+    assert formatter.partial_history == [
         ['10/01/2012', '1000.00', '', '1000.00'],
         ['13/01/2012', '2000.00', '', '3000.00'],
         ['14/01/2012', '', '500.00', '2500.00']
