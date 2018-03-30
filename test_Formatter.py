0cff12ed60b433756acb1128fefe4b6ae509405f
Noel Vock <noel1uk@gmail.com>
2018-03-30 17:14:59 +0100

    test: ensure processed_history variable reaches the right format

--- test_Formatter.py
+++ test_Formatter.py
@@ -7,14 +7,7 @@ mock_account.history = [
     ['14/01/2012', '', 500, 2500]
 ]
 
-
-def test_Formatter():
-    formatter = Formatter(mock_account.history)
-    assert formatter.formated_history == """
-    date || credit || debit || balance
-    14/01/2012 || || 500.00 || 2500.00
-    13/01/2012 || 2000.00 || || 2500.00
-    10/01/2012 || 1000.00 || || 2500.00"""
+mock_account.string = [['10/01/2012', 1000, '', 1000]]
 
 
 def test_Formatter_reverse():
@@ -35,3 +28,12 @@ def test_Formatter_format_integer():
         ['13/01/2012', '2000.00', '', '3000.00'],
         ['14/01/2012', '', '500.00', '2500.00']
     ]
+
+
+def test_Formatter_to_string():
+    formatter = Formatter(mock_account.string)
+    formatter.reverse()
+    formatter.format_integer()
+    formatter.to_string()
+    answer = "date || credit || debit || balance\n10/01/2012 || 1000.00 ||  || 1000.00"
+    assert formatter.processed_history == answer
