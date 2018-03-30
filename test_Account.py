334498d8ed64badf9b678376b8d8c25b75aa079a
Noel Vock <noel1uk@gmail.com>
2018-03-30 11:33:56 +0100

    style: add extra empty lines between methods

--- test_Account.py
+++ test_Account.py
@@ -3,28 +3,34 @@ from freezegun import freeze_time
 import datetime
 import pytest
 
-@freeze_time("2018-01-18")
 
+@freeze_time("2018-01-18")
 @pytest.fixture(scope="function")
 def account():
     account = Account()
     account.deposit(1000)
+    account.deposit(1000)
     return account
+
 
 def test_Account(account):
     assert isinstance(account, Account)
+
 
 def test_Account_deposit(account):
     account.deposit(1000)
     assert account.balance == 2000
 
+
 def test_Account_withdraw(account):
     account.withdraw(500)
     assert account.balance == 500
+
 
 @freeze_time("2018-01-18")
 def test_Account_history(account):
     account
     assert account.history == [['18/01/2018', 1000, '', 1000]]
     account.withdraw(1000)
-    assert account.history == [['18/01/2018', 1000, '', 1000], ['18/01/2018', '', 1000, 0]]
+    assert account.history == [['18/01/2018', 1000, '', 1000],
+                               ['18/01/2018', '', 1000, 0]]
