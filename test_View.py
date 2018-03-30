a09f8cfbc3977adb171b370f4968d1b114ccdda0
Noel Vock <noel1uk@gmail.com>
2018-03-30 19:08:17 +0100

    test: add test for view class

--- test_View.py
+++ test_View.py
@@ -1,18 +1,15 @@
-import pytest
 import mock
-mock_account = mock.Mock()
+from View import View
+mock_form = mock.Mock()
+mock_form.processed_history = """date || credit || debit || balance
+                10/01/2012 || 1000.00 ||  || 1000.00
+                10/01/2012 || 1000.00 ||  || 1000.00"""
 
-mock_account.history = [
-    ['10/01/2012', 1000, False, 1000],
-    ['13/01/2012', 2000, False, 3000],
-    ['14/01/2012', False, 500, 2500]
-]
 
-def test_View_prepare():
-    view = View(mock_account.history)
-    view.prepare(mock_account.history)
-    assert view.statement == """
-    date || credit || debit || balance
-    14/01/2012 || || 500.00 || 2500.00
-    13/01/2012 || 2000.00 || || 2500.00
-    10/01/2012 || 1000.00 || || 2500.00"""
+def test_View(capsys):
+    view = View(mock_form.processed_history)
+    out, err = capsys.readouterr()
+    assert out == """date || credit || debit || balance
+                10/01/2012 || 1000.00 ||  || 1000.00
+                10/01/2012 || 1000.00 ||  || 1000.00
+"""
